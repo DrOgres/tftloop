c1e26ab79e80e20330195ec02ca77c171e045da6
DrOgres <drogres@gmail.com>
2020-12-22 21:13:23 -0600

    config

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// Less configuration
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function(cb) {
+  gulp
+    .src('less/tftloop.less')
+    .pipe(less())
+    .pipe(
+      gulp.dest("./")); 
+  cb();
+});
+
+gulp.task(
+  'default',
+  gulp.series('less', function(cb) {
+    gulp.watch('less/*.less', gulp.series('less'));
+    cb();
+  })
+);
